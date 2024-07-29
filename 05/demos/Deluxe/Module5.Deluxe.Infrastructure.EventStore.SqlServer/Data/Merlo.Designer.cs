bf6f3188158b850171ab95ebbca88c34934d4ca8
sjaiswalau <mailsidjaiswal@gmail.com>
2024-07-30 00:37:45 +1000

    regular - working

--- 05/demos/Deluxe/Module5.Deluxe.Infrastructure.EventStore.SqlServer/Data/Merlo.Designer.cs
+++ 05/demos/Deluxe/Module5.Deluxe.Infrastructure.EventStore.SqlServer/Data/Merlo.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'E:\My Projects\Pluralsight\CQRS\Impl\Source\Module5\Deluxe\Module5.Deluxe.Infrastructure.EventStore.SqlServer\Data\Merlo.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Sid\Work\Pluralsight\modern-software-architecture-domain-models-cqrs-event-sourcing\05\demos\Deluxe\Module5.Deluxe.Infrastructure.EventStore.SqlServer\Data\Merlo.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
--- 05/demos/Premium/Module5.Premium.Infrastructure/Merlo.Designer.cs
+++ 05/demos/Premium/Module5.Premium.Infrastructure/Merlo.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'E:\My Projects\Pluralsight\CQRS\Impl\Source\Module5\Premium\Module5.Premium.Infrastructure\Merlo.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Sid\Work\Pluralsight\modern-software-architecture-domain-models-cqrs-event-sourcing\05\demos\Premium\Module5.Premium.Infrastructure\Merlo.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
